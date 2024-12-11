d54663676b83d8cffbbb35e0eedc05d2099dd8e5
Yusuke Kawamoto <novogrammer@gmail.com>
2024-12-11 19:24:50 +0900

    maplibre-glを導入

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
+
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>Hello</div>
+  <div id="map"></div>
 `;
+
+
+const map = new maplibregl.Map({
+  container: 'map',
+  style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9 // starting zoom
+});
+
