1a9a7e9cf1aaae47e555a231ba2a3cc1b68928c9
Yusuke Kawamoto <novogrammer@gmail.com>
2024-12-13 17:06:26 +0900

    sticky確認

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
-import './style.css'
+import './style.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div id="map"></div>
+  <section class="p-section-hero">
+    <h1 class="p-section-hero__title">Hero</h1>
+  </section>
+  <div class="l-scroll">
+    <div  class="l-scroll__sticky">
+      <div id="map"></div>
+    </div>
+    <div class="l-scroll__static">
+      <section class="p-section-intro">
+        <h2 class="p-section-intro__title">Intro</h2>
+      </section>
+      <section class="p-section-about">
+        <h2 class="p-section-about__title">About</h2>
+      </section>
+    </div>
+  </div>
+  <section class="p-section-contact">
+    <h2 class="p-section-contact__title">Contact</h2>
+  </section>
 `;
 
 
