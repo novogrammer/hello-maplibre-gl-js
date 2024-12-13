17236c0ae2078ee8055c48ea036a4570ca6066b6
Yusuke Kawamoto <novogrammer@gmail.com>
2024-12-13 17:07:18 +0900

    無難な地図を検討

--- src/main.ts
+++ src/main.ts
@@ -28,8 +28,31 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 const map = new maplibregl.Map({
   container: 'map',
-  style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  style:"https://tile2.openstreetmap.jp/styles/osm-bright/style.json",
+  // style: {
+  //     version: 8,
+  //     sources: {
+  //         rtile: {
+  //             type: 'raster',
+  //             tiles: [
+  //                 'https://tile.openstreetmap.jp/{z}/{x}/{y}.png',
+  //             ],
+  //             tileSize: 256,
+  //             attribution:
+  //                 '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  //         },
+  //     },
+  //     layers: [
+  //         {
+  //             id: 'raster-tiles',
+  //             type: 'raster',
+  //             source: 'rtile',
+  //             minzoom: 0,
+  //             maxzoom: 22,
+  //         },
+  //     ],
+  // },
+  center: [135.4948501,34.6856111], // starting position [lng, lat]
+  zoom: 18 // starting zoom
 });
 
